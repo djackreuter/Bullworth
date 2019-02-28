053eee938063b6ee2d76eae384824370d7c61284
Dylan Reuter <djreuter45@gmail.com>
2019-02-28 16:12:21 -0600

    added routes for missions and locations

--- routes/api/locations.js
+++ routes/api/locations.js
@@ -1,2 +1,101 @@
 const express = require('express');
 const router = express.Router();
+const { Location } = require('../../models/Location');
+const { isEmpty } = require('../../validation/isEmpty');
+const mongoose = require('mongoose')
+
+/**
+ * @route get /api/locations
+ * @desc get all locations
+ * @access public
+ */
+router.get('/', async (req, res) => {
+  try {
+    const locations = await Location.find();
+    if (!locations) {
+      return res.status(404).json({ error: "No locations found" });
+    }
+    return res.json(locations);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+/**
+ * @route get /api/locations/:id
+ * @desc get one locations
+ * @access public
+ */
+router.get('/:id', async (req, res) => {
+  try {
+    const location = await Location.findById(req.params.id);
+    if (!location) {
+      return res.status(404).json({ error: "Location not found" });
+    }
+    return res.json(location);
+  } catch (err) {
+    return res.status(400).json(err);
+  }
+});
+
+/**
+ * @route post /api/locations
+ * @desc create a location
+ * @access public
+ */
+router.post('/', (req, res) => {
+  const { name } = req.body;
+  if (isEmpty(name)) {
+    return res.status(400).json({ error: "Name cannot be blank" });
+  }
+  const newLocation = {
+    _id: mongoose.Types.ObjectId,
+    name
+  }
+  // dont actually save to db, just return new obj
+  return res.json(newLocation);
+});
+
+/**
+ * @route patch /api/locations/:id
+ * @desc update a location
+ * @access public
+ */
+router.patch('/:id', async (req, res) => {
+  const { name } = req.body;
+  if (isEmpty(name)) {
+    return res.status(400).json({ error: "Name cannot be blank" });
+  }
+  try {
+    const location = await Location.findById(req.params.id);
+    if (!location) {
+      return res.status(404).json({ error: "Location not found" });
+    }
+    const newLocation = {
+      _id: location._id,
+      name
+    }
+    // dont actually save to db, just return new obj
+    return res.json(newLocation);
+  } catch (err) {
+    return res.status(400).json(err);
+  }
+});
+
+/**
+ * @route delete /api/locations
+ * @desc delete a location
+ * @access public
+ */
+router.delete('/:id', async (req, res) => {
+  try {
+    const location = await Location.findById(req.params.id);
+    if (!location) {
+      return res.status(404).json({ error: "Location not found" });
+    }
+    // dont actually delete from db, just return 
+    return res.json({ success: true });
+  } catch (err) {
+    return res.status(400).json(err);
+  }
+});
--- routes/api/missions.js
+++ routes/api/missions.js
@@ -1,2 +1,100 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const { Misson } = require('../../models/Mission');
+const { isEmpty } = require('../../validation/isEmpty');
+
+/**
+ * @route GET /api/missions/
+ * @desc get all missions
+ * @access public
+ */
+router.get('/', async (req, res) => {
+  try {
+    const missions = await Mission.find();
+    if (!missions) {
+      return res.status(404).json({ error: "No missions found" });
+    }
+    return res.json(missions);
+  } catch (err) {
+    return res.status(400).json(err);
+  }
+});
+
+ /**
+ * @route GET /api/missions/:id
+ * @desc GET single mission
+ * @access public
+*/
+router.get('/:id', async (req, res) => {
+  try {
+    const mission = await Misson.findById(req.params.id);
+    if (!mission) {
+      return res.status(404).json({ error: "Could not find mission" });
+    }
+    return res.json(mission);
+  } catch (err) {
+    return res.status(400).json(err);
+  }
+});
+
+ /**
+ * @route POST /api/missions/
+ * @desc create a mission
+ * @access public
+ */
+router.post('/', (req, res) => {
+  const { name } = req.body;
+  if (isEmpty(name)) {
+    return res.status(400).json({ error: "Name cannot be empty" });
+  }
+  const newMission = {
+    _id: mongoose.Types.ObjectId,
+    name
+  }
+  // dont save to db, just return new obj as if it did
+  return res.json(newMission);
+});
+
+ /**
+ * @route PATCH /api/missions/:id
+ * @desc update a mission
+ * @access public
+ */
+router.patch('/:id', async (req, res) => {
+  const { name } = req.body;
+  if (isEmpty(name)) {
+    return res.status(400).json({ error: "Name cannot be empty" });
+  }
+  try {
+    const mission = await Misson.findById(req.params.id);
+    if (!mission) {
+      return res.status(404).json({ error: "Mission not found" });
+    }
+    const updatedMission = {
+      _id: mission._id,
+      name
+    }
+    // dont actually update, just return as if it did
+    return res.json(updatedMission);
+  } catch (err) {
+    return res.status(400).json(err);
+  }
+});
+
+ /**
+ * @route DELETE /api/missions/:id
+ * @desc delete a mission
+ * @access public
+ */
+router.delete('/:id', async (req, res) => {
+  try {
+    const mission = await Mission.findById(req.params.id);
+    if (!mission) {
+      return res.status(404).json({ error: "Mission not found" });
+    }
+    return res.json({ success: true });
+  } catch (err) {
+    return res.status(400).json(err);
+  }
+});
